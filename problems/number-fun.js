da315477a80286a404f542e64d1f081d399691a2
davigravi <davidlee777@gmail.com>
2021-08-19 17:31:07 -0500

    phase 2 solved

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -3,8 +3,19 @@ function returnsThree() {
 }
 
 function reciprocal(n) {
-  // Your code here
+  if (n < 1 || n > 1000000) {
+    throw TypeError('this number is not between 1 and 1000000')
+  }
+  let numString = n.toString();
+  let newNum = '';
+  for (let i = numString.length - 1; i >= 0; i--) {
+    let num = numString[i];
+    newNum += num;
+  }
+  return Number(newNum);
 }
+
+console.log(reciprocal(20000000))
 
 module.exports = {
   returnsThree,
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,13 +1,35 @@
 const chai = require('chai');
 const expect = chai.expect;
-const {returnsThree} = require('../problems/number-fun')
+const { returnsThree, reciprocal } = require('../problems/number-fun')
 
-describe('returnsThree()', function() {
-    it('should return the number 3', function() {
+describe('returnsThree()', function () {
+    it('should return the number 3', function () {
         expect(returnsThree()).to.equal(3);
     })
 })
 
-describe('', function() {
+describe('reciprocal()', function () {
+    context('if num is between 1 and 1000000', function () {
+        it('should return reciprocal of num', function () {
+            let num1 = 1;
+            let num2 = 154;
+
+            expect(reciprocal(num1)).to.equal(1);
+            expect(reciprocal(num2)).to.equal(451);
+        })
+    })
+    context('if num is outside 1 and 1000000', function () {
+        it('it should throw a TypeError', function () {
+            
+            let num1 = 0;
+            let num2 = 1000001;
+            // let result1 = reciprocal(num1)
+
+            expect(result1).to.throw(TypeError, 'this number is not between 1 and 1000000');
+            expect(reciprocal(num1)).to.throw(TypeError);
+            expect(reciprocal(num2)).to.throw(TypeError);
+
+        })
+    })
 
 })
