ac35a70d27d60a90887634f3af1ebee00aeeb9fb
tannerhladek <tanner.hladek@icloud.com>
2021-08-19 16:12:39 -0700

    unfinished myMap test spec and myMay function file

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,16 @@
 function myMap(inputArray, callback) {
-  // Your code here
+
+  if (!Array.isArray(inputArray)) {
+    throw TypeError('input should be an array');
+  }
+  let res = [];
+  inputArray.forEach(function(ele) {
+    let newNum = callback(ele);
+    res.push(newNum);
+  })
+  return res;
 }
+
+console.log(myMap([1, 2, 3], (el) => el * 2))
 
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,24 @@
-// Your code here
+const chai = require('chai');
+const {expect} = require('chai');
+const spies = require('chai-spies');
+const myMap = require('../problems/my-map');
+
+describe('myMap()', function() {
+    context('if input is not an array', function() {
+        it('should throw an Error', function() {
+            let arr = 'not array';
+
+            expect(() => myMap(arr)).to.throw(TypeError);
+        })
+    })
+
+    context('if input is array', function() {
+        it('should return new array with each element passed to callback', function() {
+            let arr = [1, 2, 3];
+            let cb = (el) => el * 2;
+            expect(arr).to.equal([1, 2, 3]);
+            expect(myMap(arr, cb)).to.eql([2, 4, 6]);
+        })
+    })
+
+})
