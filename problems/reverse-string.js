d9013010cedd4cf58b10ab0581dc9486dd83c752
davigravi <davidlee777@gmail.com>
2021-08-19 15:55:45 -0500

    phase 1 solved

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,12 @@
 module.exports = function reverseString(string) {
-  // Your code here
+  // let newString = '';
+  // for (let i = string.length-1; i >= 0; i--) {
+  //   let char = string[i];
+  //   newString += char;
+  // }
+  // return newString;
 };
+
+
+
+
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,10 @@
-// Your code here
+const {expect} = require('chai');
+const {reverseString} = require('../problems/reverse-string')
+
+
+describe ("reverseString", function (){
+    it ("should reverse string", function (){
+        let res = reverseString("fun");
+        expect(res).to.equal("nuf");
+    })
+})
